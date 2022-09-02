9539747f466878886aa0365eef9b6e03570cb6e4
Adrian Wong <adrianwjw@gmail.com>
2022-09-02 17:16:33 +1000

    Build Mercury cross-compiler

--- bin/install-then-build.sh
+++ bin/install-then-build.sh
@@ -41,3 +41,38 @@ if [ ! -f "$OUT_DIR/$TARBALL" ] ; then
     ( mkdir -p "$OUT_DIR" &&
       tar zcf "$OUT_DIR/$TARBALL" -C /tmp "mercury-rotd-$MERCURY_ROTD" )
 fi
+
+# Build aarch64 Mercury.
+rm -rf "$SRC_DIR"
+tar xvf "$DLD_DIR/rotd-$MERCURY_ROTD.tar.gz" -C "$DLD_DIR"
+
+export PATH="/tmp/mercury-rotd-$MERCURY_ROTD/bin:$PATH"
+export CC="clang -target aarch64-apple-darwin"
+AARCH64_TRIPLE=aarch64-apple-darwin
+
+pushd "$SRC_DIR"
+
+# Manually overwrite the older version of configure_cross for now.
+# Remove once we are using ROTD 2022-08-18 or newer.
+cp "$CWD/bin/configure-cross-5f1dfc806f87" tools/configure_cross
+./tools/configure_cross \
+    --host=aarch64-apple-darwin \
+    --enable-libgrades=hlc.par.gc \
+    --prefix="/tmp/mercury-rotd-$MERCURY_ROTD-$AARCH64_TRIPLE"
+
+mmake depend
+mmake MMAKEFLAGS="$PARALLEL"
+mmake install MMAKEFLAGS="$PARALLEL"
+
+./tools/copy_mercury_binaries \
+    "/tmp/mercury-rotd-$MERCURY_ROTD" \
+    "/tmp/mercury-rotd-$MERCURY_ROTD-$AARCH64_TRIPLE"
+
+popd
+
+AARCH64_TARBALL=mercury-rotd-$MERCURY_ROTD-$AARCH64_TRIPLE-osx.tar.gz
+
+if [ ! -f "$OUT_DIR/$AARCH64_TARBALL" ] ; then
+    ( mkdir -p "$OUT_DIR" && tar zcf "$OUT_DIR/$AARCH64_TARBALL" \
+      -C /tmp "mercury-rotd-$MERCURY_ROTD-$AARCH64_TRIPLE" )
+fi
